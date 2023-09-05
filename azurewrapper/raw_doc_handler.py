95dcf3b371c2c2a06863afe8423db194b2da42bb
Tommy Guy <riguy@microsoft.com>
2023-09-05 06:38:23 -0700

    minor refactor

--- azurewrapper/raw_doc_handler.py
+++ azurewrapper/raw_doc_handler.py
@@ -4,13 +4,11 @@ from datetime import datetime
 from typing import Dict
 
 from indexgen.localtypes import SecDocRssEntry, serialize_doc_entry
+from .doc_handler_base import AzureBlobHandlerBase
 
-class AzureRawDocsBlobHandler:
-    def __init__(self):
-        self.connection_string = os.environ['DocumentBlobConnectionString']
-        self.container_name = os.environ['DocumentBlobContainer']
-        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
-        self.container_client = self.blob_service_client.get_container_client(self.container_name)
+class AzureRawDocsBlobHandler(AzureBlobHandlerBase):
+
+    container_name = "RawDocumentBlobContainer"
 
     def upload_files(self, sec_entry : SecDocRssEntry, local_files : Dict[str, str]):
         root = self._build_root_path(sec_entry)
@@ -26,10 +24,6 @@ class AzureRawDocsBlobHandler:
         blob_client.upload_blob(serialize_doc_entry(sec_entry), overwrite=True)
         return summary_path
     
-    def get_path(self, remote_path) -> str:
-        blob_stream = self.container_client.download_blob(remote_path)
-        blob_stream.readall().decode('utf-8', 'ignore')
-
     def _build_root_path(self, sec_entry : SecDocRssEntry):
         # todo check if exists. roll counter if so.
         return f"{sec_entry.cik}/{datetime.strftime(sec_entry.published, '%Y%m%d')}/{sec_entry.doc_type}_0"
--- azurewrapper/raw_doc_queue.py
+++ azurewrapper/raw_doc_queue.py
@@ -5,18 +5,21 @@ from azure.storage.queue import QueueServiceClient, QueueMessage
 class AzureQueueWriter:
     def __init__(self):
         self.connection_string = os.environ['DocumentBlobConnectionString']
-        self.queue_name = os.environ['DocumentQueueName']
+        self.queue_name = os.environ['DocumentRawProcessQueueName']
+        self.extract_table_queue_name = os.environ['DocumentRawExtractStructuredDataQueueName']
         queue_service_client = QueueServiceClient.from_connection_string(self.connection_string)
         self.queue_client = queue_service_client.get_queue_client(self.queue_name)
+        self.extract_table_q_client = queue_service_client.get_queue_client(self.extract_table_queue_name)
 
     def write_message(self, message):
         self.queue_client.send_message(message)
+        self.extract_table_q_client.send_message(message)
 
-    def pop_message(self) -> QueueMessage:
+    def pop_doc_parse_message(self) -> QueueMessage:
         """Assume that the upstream system will requeue if this message is a problem."""
         msg = self.queue_client.receive_message(visibility_timeout=120)
         if msg:
             return msg
         
-    def delete_message(self, msg : QueueMessage):
+    def delete_doc_parse_message(self, msg : QueueMessage):
         self.queue_client.delete_message(msg)
