0db56c1c6e64aecd4d6762cb9d0262fe876c68ca
Tommy Guy <riguy@microsoft.com>
2023-09-12 09:33:37 -0700

    table parser

--- docparsers/workiva.py
+++ docparsers/workiva.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
-from typing import List
-import json
+from typing import List, Dict
 
 class WorkivaParser:
 
@@ -8,6 +7,7 @@ class WorkivaParser:
         string_elts = []
         
         stack = [dom]
+        context_dict = {}
 
         while len(stack):
             elt = stack.pop()
@@ -19,9 +19,9 @@ class WorkivaParser:
                 continue
 
             if elt.name == 'header' and elt.prefix == 'ix':
-                pass  # todo handle the full header as a data element only. Save the references somewhere.
+                context_dict = self._clean_context(elt)  # todo handle the full header as a data element only. Save the references somewhere.
             elif elt.name == "table":
-                table_content = self._clean_table(elt)
+                table_content = self._clean_table(elt, context_dict)
                 string_elts.append(table_content)
             elif elt.name == 'a':
                 string_elts.append(elt.get_text())
@@ -55,9 +55,34 @@ class WorkivaParser:
         
         return [s for s in string_elts if s]
 
-    def _clean_table(self, table : BeautifulSoup) -> str:
-        if len(table.find_all('ix:nonFraction')) > 0:
-            return self._clean_structured_table(table)
+    def _clean_context(self, elt : BeautifulSoup) -> Dict[str, any]:
+        ret_dict = {}
+
+        resources = elt.find_next(lambda x: x.name == 'resources' and x.prefix == 'ix')
+        for child in resources.children:
+            if child.name == 'context':
+                child_id = child.attrs['id']
+                
+                # try to parse a period
+                periods = child.find_all('period')
+                if len(periods) != 1:
+                    raise NotImplementedError(f"Resource {child}")
+                period : BeautifulSoup = periods[0]
+                value_dict = {}
+                for period_elt in period.children:
+                    if period_elt.name is not None:
+                        value_dict[period_elt.name] = period_elt.get_text()
+
+                ret_dict[child_id] = value_dict    
+            elif child.name == 'unit':
+                # TODO: also parse units.
+                pass
+        
+        return ret_dict
+
+    def _clean_table(self, table : BeautifulSoup, context_table: Dict[str, any]) -> str:
+        if len(table.find_all(lambda x: x.prefix == 'ix')) > 0:
+            return self._clean_structured_table(table, context_table)
         else:
             return self._clean_simple_table(table)
 
@@ -77,39 +102,75 @@ class WorkivaParser:
 
         return str(table)
 
-    def _clean_structured_table(self, table : BeautifulSoup) -> str:
+    def _clean_structured_table(self, table : BeautifulSoup, context_table: Dict[str, any]) -> str:
         # remove style from all elements in the table.
         # todo drop (Amounts in millions except per share amounts)
         data_elts = []
         stack = [table]
+
         while len(stack):
             elt = stack.pop()
-            if elt.name == 'table':
+            if elt.name is None:
+                pass
+            elif elt.name == 'table':
                 stack.extend(reversed(list(elt.children)))
-            if elt.name == 'tr':
-                # two cases:
-                    # something has an ix. parse entire row as that, keeping the initial non-empty col as a key.
-                    # nothing does. ignore.
-                stack.extend(reversed(list(elt.children)))
-            elif elt.name == 'td':
+            elif elt.name == 'tr':
                 ix_elts = elt.find_all(lambda z: z.prefix == 'ix')
-                if len(ix_elts) == 1:
-                    structured_data = self._get_single_ix_elt(elt)
-                    import ipdb; ipdb.set_trace()
-                elif len(ix_elts) > 1:
-                    raise ValueError(f"too many ix elts in a td: {elt}")
+                if len(ix_elts) > 0:
+                    # case 1: use parser
+                    data_elts.extend(self._parse_structured_tr(elt, context_table))
                 else:
-                    elt.attrs = {}
+                    pass
+            else:
+                raise ValueError(f"Unexpected elt in structured table parser: {elt}")
                     
-                # if you have an ix namespace child just use it.
-                # if not then use get_text
-
-        # make a table
+        # make a table from data_elts.
+        
         return data_elts
     
-    def _get_single_ix_elt(self, elt):
+    def _parse_structured_tr(self, tr : BeautifulSoup, context_table: Dict[str, any]):
+        """Assume we have a tr element. Parse out possible key (first non-ix containing) and all values."""
+        current_label = ''
+        structured_data_elements = []
+        
+        for td in tr.children:  # assuming well-formatted here. could verify all children are td.
+            if td.name is None:
+                continue
+
+            ix_elts = td.find_all(lambda z: z.prefix == 'ix')
+            if len(ix_elts) >= 1:
+                for ix_elt in ix_elts:
+                    structured_data = self._get_single_ix_elt(ix_elt)
+                    structured_data.update(context_table[structured_data['contextRef']])
+                    structured_data_elements.append(structured_data)
+            else:
+                candidate_label = td.get_text()
+                current_label = candidate_label if len(candidate_label) > len(current_label) else current_label
+
+        # apply_label to each row.
+        for elt in structured_data_elements:
+            elt['rowLabel'] = current_label
+
+        return structured_data_elements
+
+    def _get_single_ix_elt(self, elt : BeautifulSoup):
         """Clean and return structured data"""
-        import ipdb; ipdb.set_trace()
+        vals = {
+            'eltName': elt.name,
+            'contextRef': elt.attrs.get('contextRef'),
+            'ixName': elt.attrs.get('name'),
+            'scale': float(elt.attrs.get('scale', 1)),
+            'decimals': elt.attrs.get('decimals', 0),
+            'id': elt.attrs.get('id'),
+            'unit': elt.attrs.get('unitRef'),
+            'format': elt.attrs.get('format')
+        }
+        vals['raw_value'] = elt.get_text()
 
-
-
+        try:
+            raw_value = float(elt.get_text().replace(',', ''))
+        except ValueError:
+            vals['value'] = vals['raw_value']
+        else:
+            vals['value'] = (10**vals['scale']) * raw_value
+        return vals
