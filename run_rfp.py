e2592b971d6e398caa11e670ba488735faeb1e13
Tommy Guy <riguy@microsoft.com>
2023-10-19 11:36:24 -0700

    add example

--- run_rfp.py
+++ run_rfp.py
@@ -2,4 +2,5 @@ from rfp.driver import RfpDriver
 
 
 r = RfpDriver()
-r.run("./samples/RFP/NewBrunswick/rfp_rawtext.txt")
+r.run("./samples/RFP/UW1/main_rfp.txt")
+#r.run("./samples/RFP/NewBrunswick/rfp_rawtext.txt")
