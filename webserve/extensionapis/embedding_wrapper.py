06574d9424f78e12e2c71af55b361bdbe9e3d45f
Tommy Guy <riguy@microsoft.com>
2023-11-30 08:33:14 -0800

    milvus

---
+++ webserve/extensionapis/embedding_wrapper.py
@@ -0,0 +1,18 @@
+from pymilvus import Collection, connections
+
+
+class _MilvusWrapperSingleton:
+
+    def __init__(self) -> None:
+        connections.connect(
+            alias="default", # database 
+            user='username', # get from setting
+            password='password', # get from setting
+            host='localhost', # get from setting
+            port='19530'
+        )
+        collection = Collection()
+
+
+
+milvus_wrapper = _MilvusWrapperSingleton()
