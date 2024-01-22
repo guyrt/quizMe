3679e14cbe5afd86d8d4ee1d24111f0bb18ee3a9
Tommy Guy <riguy@microsoft.com>
2024-01-22 15:41:37 -0800

    squasH

--- browser_extension/src/globalSettings.ts
+++ browser_extension/src/globalSettings.ts
@@ -1,1 +1,2 @@
-export const domain = "http://localhost:8000";
+//export const domain = "http://localhost:8000";
+export const domain = "https://ca-web.gentlebush-c9046e72.westus3.azurecontainerapps.io";
