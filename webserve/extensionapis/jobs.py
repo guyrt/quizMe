074afe1d4c2593a2327b0fe0d141c706cedd9079
Tommy Guy <riguy@microsoft.com>
2024-02-08 21:07:05 -0800

    breakfix

--- webserve/extensionapis/jobs.py
+++ webserve/extensionapis/jobs.py
@@ -1,6 +1,6 @@
 from django_rq import job
 
-from webserve.azurewrapper.freeassociate.rawdoc_handler import RawDocCaptureHander
+from azurewrapper.freeassociate.rawdoc_handler import RawDocCaptureHander
 
 @job
 def clean_raw_doc_capture(location_container : str, location_path : str):
