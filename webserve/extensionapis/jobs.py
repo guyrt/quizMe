0867e00fece4fc236c199403daba780399aa6e50
Tommy Guy <riguy@microsoft.com>
2024-02-21 14:26:13 -0800

    minor

--- webserve/extensionapis/jobs.py
+++ webserve/extensionapis/jobs.py
@@ -5,4 +5,5 @@ from azurewrapper.freeassociate.rawdoc_handler import RawDocCaptureHander
 @job
 def clean_raw_doc_capture(location_container : str, location_path : str):
     """Clean up an old file"""
-    RawDocCaptureHander(location_container).delete(location_path)
+    if location_container != "" and location_path != "":
+        RawDocCaptureHander(location_container).delete(location_path)
