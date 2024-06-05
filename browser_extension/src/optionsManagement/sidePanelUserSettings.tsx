0a3957a391e7ca7a881edb48771473c108ee141f
RanieryMendes <mendrc18@wfu.edu>
2024-06-05 00:54:20 -0400

    Add button to sidePanel and loggedInSettings

--- browser_extension/src/optionsManagement/sidePanelUserSettings.tsx
+++ browser_extension/src/optionsManagement/sidePanelUserSettings.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { SharedStateReaders } from "../stateTrackers/sharedStateReaders";
-import { fsm } from "../stateTrackers/sidePanelThread/sidePanelStateMachine";
+import { fsm, SidePanelState } from "../stateTrackers/sidePanelThread/sidePanelStateMachine";
 import { SidePanelUserSettingsQuizHistory } from "./sidePanelUserSettingsQuizHistory";
 import { BlockedDomains } from "./blockedDomains";
 
@@ -28,6 +28,15 @@ export function SidePanelLoggedInUserSettings() {
         })
     }, []);
 
+    function handleReturnClick() {
+       
+        // Need to find a way of maybe using the update state function. 
+        //An idea to try tmr:  store the previous state prior to setting the state to "showSetting"
+        //on click, update state back to that previous one; 
+        //fsm.subscribe();
+        //console.log 
+    }
+
     return (
         <>
             <SidePanelUserSettingsQuizHistory />
@@ -35,7 +44,10 @@ export function SidePanelLoggedInUserSettings() {
             <Checkbox label="Track all pages, not just articles?" getter={sharedStateReader.getTrackAllPages} setter={setFilterToArticles} />
             <br/>
             <BlockedDomains />
-            <button id='logout' onClick={logoutThisDevice}>Log out</button>
+            <div id="buttonsWrapper" className="buttonSettingsWrap">
+                <button id='back' className="buttonSettings" onClick={handleReturnClick}>Return Home</button>
+                <button id='logout' className="buttonSettings" onClick={logoutThisDevice}>Log out</button>
+            </div>
         </>
     )
 }
